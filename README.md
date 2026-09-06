e5f63e8491f268c45d7c1fa242d286cc3ddbdef5
Omar Nieto Crisóstomo <210950999+OmarNietoUACM@users.noreply.github.com>
2026-09-06 11:45:12 -0600

    Se modificó la segunda Activity para agregar botones con distintas formas del uso de Intent implicitos

--- README.md
+++ README.md
@@ -10,12 +10,13 @@ los compara con valores guardados como constantes en on companion object.
 El ejercicio se elaboró para mostrar el uso del diseño con LinearyLayout
 y ConstraintLayout; también se mostró el uso de Intent Explicitos donde se 
 abre la segunda Activity. En la segunda Activity, se programó 
-el botón para los siguientes acciones:
-- Regresar a la Activity previa: temas tratados Tareas y la pila de activities
-- Enviar datos a otra Aplicación: temas tratados Intent Implicitos
-- Se enviaron datos para abir el navegador por defecto
-- Se enviaron datos para compartirlo a WhatsApp
-- Se empleo el Agente de Resolución de Intent y Android Sharesheet
+notones para los siguientes acciones:
+- Regresar a la Activity previa:  (temas: Task y la pila de activities)
+- Enviar datos a otra Aplicación Agente de resolución de Intents: (temas tratados Intent Implicitos)
+- Se enviaron datos para abrir el navegador y compartir en WhatsApp Web 
+- Se enviaron datos para compartirlo WhatsApp
+- Se emplea el Agente de Resolución de Intent 
+- Se emplea Android Sharesheet
 
 La aplicación utiliza los controles: **LinearLayout**, **TextView**, **EditText**, **Button**, **Toast**.
 Se utilizaron los objetos **Intent** Implicitos y Explicitos
--- app/src/main/java/com/example/a2026_ii_comunicacionestreactivities/PricipalActivity.kt
+++ app/src/main/java/com/example/a2026_ii_comunicacionestreactivities/PricipalActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,49 +19,87 @@ class PricipalActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
                 setContentView(R.layout.activity_pricipal)
 
+        val mensaje = findViewById<TextView>(R.id.tv_mesaje)
+        if(intent != null)
+        {
+            //Se recibe los datos del Intent
+            val login = intent.getStringExtra(MainActivity.MAIN_LOGIN_KEY)
+            val passwd = intent.getStringExtra(MainActivity.MAIN_PASSWD_KEY)
+            if (!login.equals(PricipalActivity.login) || !passwd.equals(PricipalActivity.passwd))
+                mensaje.text = "El usuario o la contraseña son incorrectos"
+            else
+                mensaje.text = "Has inciado seción!"
+        }
+
+        //-------------------------------------------------
+        //Se cierra la Activity y regresa a la Activity previa en el Stack
+        //-------------------------------------------------
         val boton = findViewById<Button>(R.id.boton2)
         boton.setOnClickListener {
-            //Regrea a la actividad anterior
-            //finish()
+            finish()
+        }
 
+        //---------------------------------------------
+        //Se abre la p+agina web de WhatsApp para enviar
+        // o compartir texto o imágenes
+        //---------------------------------------------
+        val botonWhatsAppWeb = findViewById<Button>(R.id.botonWhatsAppWeb)
+        botonWhatsAppWeb.setOnClickListener {
 
-            
             // Se abre el navegador con la pagina de WhatsApp
-            /*
-            val numero = "5512345678"
+            val numero = "5512345678" //Número de registro de WhatsApp
             val uri = Uri.parse("https://wa.me/" + numero)
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
+        }
 
-             */
+        //---------------------------------------------
+        //Se abre directamente la aplicación de WhatsApp para enviar
+        // o compartir texto o imágenes
+        //---------------------------------------------
+        val botonWhatsApp = findViewById<Button>(R.id.botonWhatsApp)
+        botonWhatsApp.setOnClickListener {
 
-            //Abre directamente la aplicación de WhatsApp para enviar el texto
-            /*
-            val sendIntent =  Intent(Intent.ACTION_SEND)
-            sendIntent.putExtra(Intent.EXTRA_TEXT, "Este el el texto que se enviará a la aplicación.")
+            val sendIntent = Intent(Intent.ACTION_SEND)
+            sendIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Este el el texto que se enviará a la aplicación."
+            )
             sendIntent.setType("text/plain")
             sendIntent.setPackage("com.whatsapp")
-            startActivity(sendIntent);
-             */
+            if (sendIntent.resolveActivity(packageManager) != null)
+                startActivity(sendIntent)
+            else Toast.makeText(this,
+                "La aplicción WhatsApp no está instalada",
+                Toast.LENGTH_LONG).show()
 
+        }
 
-            //Si varias aplicaciones coinciden con la acción ACTION_SEND
-            // Se abrirá el dialogo Agente de Resolución de Intent para
-            // que se seleccione una aplicación destino
-            //En el siguiente código se muestra este ejemplo
-            /*
+        //---------------------------------------------
+        //Si varias aplicaciones coinciden con la acción ACTION_SEND
+        // Se abrirá el dialogo Agente de Resolución de Intent para
+        // que se seleccione una aplicación destino
+        //En el siguiente código se muestra este ejemplo
+        //---------------------------------------------
+        val botonSendAgen = findViewById<Button>(R.id.botonSendAgen)
+        botonSendAgen.setOnClickListener {
             val sendIntent =  Intent(Intent.ACTION_SEND)
             sendIntent.putExtra(Intent.EXTRA_TEXT, "Este el el texto que se enviará a la aplicación.")
             sendIntent.setType("text/plain")
             startActivity(sendIntent)
-             */
+        }
 
+        //---------------------------------------------
+        //Se puede utilizar Android Sharesheet
+        // en lugar del Agente de Resolución de Intent
+        // para seleccionar una aplicación destino
+        //tiene mejores funcionalidades para desarrolladores
+        //En el siguiente código se muestra este ejemplo
+        //---------------------------------------------
 
-            //Se puede utilizar Android Sharesheet
-            // en lugar del Agente de Resolución de Intent
-            // para seleccionar una aplicación destino
-            //tiene mejores funcionalidades para desarrolladores
-            //En el siguiente código se muestra este ejemplo
+        val botonSharesheet = findViewById<Button>(R.id.botonSharesheet)
+        botonSharesheet.setOnClickListener {
+
             val sendIntent =  Intent(Intent.ACTION_SEND)
             sendIntent.putExtra(Intent.EXTRA_TEXT, "Este es el texto que se debe enviar")
             sendIntent.setType("text/plain")
@@ -68,19 +107,6 @@ class PricipalActivity : AppCompatActivity() {
             var shareIntent = Intent.createChooser(sendIntent, null)
             startActivity(shareIntent);
 
-        }
-
-        val mensaje = findViewById<TextView>(R.id.tv_mesaje)
-
-        if(intent != null)
-        {
-            //Se recibe el Intent
-            val login = intent.getStringExtra(MainActivity.MAIN_LOGIN_KEY)
-            val passwd = intent.getStringExtra(MainActivity.MAIN_PASSWD_KEY)
-            if (!login.equals(PricipalActivity.login) || !passwd.equals(PricipalActivity.passwd))
-                mensaje.text = "El usuario o la contraseña son incorrectos"
-            else
-                mensaje.text = "Has inciado seción!"
         }
 
 
